229b116b66b31d5fbafe8738fa51eec1138d756c
Emma Walker <emmmarosewalker@gmail.com>
2019-03-26 19:48:30 +1100

    Debugging

--- src/components/contact.js
+++ src/components/contact.js
@@ -19,24 +19,24 @@ export default class Contact extends React.Component {
 
   render() {
     return (
-<form method="post" action="#">
-    <div className="field half first">
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
-    </div>
-    <div className="field half">
-        <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" />
-    </div>
-    <div className="field">
-        <label htmlFor="message">Message</label>
-        <textarea name="message" id="message" rows="6"></textarea>
-    </div>
-    <ul className="actions">
-        <li><input type="submit" value="Send Message" className="special" /></li>
-        <li><input type="reset" value="Clear" /></li>
-    </ul>
-</form>
+        <form method="post" action="#">
+            <div className="field half first">
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" />
+            </div>
+            <div className="field half">
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" id="email" />
+            </div>
+            <div className="field">
+                <label htmlFor="message">Message</label>
+                <textarea name="message" id="message" rows="6"></textarea>
+            </div>
+            <ul className="actions">
+                <li><input type="submit" value="Send Message" className="special" /></li>
+                <li><input type="reset" value="Reset" /></li>
+            </ul>
+        </form>
     )
   }
 }

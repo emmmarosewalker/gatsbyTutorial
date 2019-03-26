b3589294875ba24a1874e37c3b624a317af05027
Emma Walker <emmmarosewalker@gmail.com>
2019-03-26 19:51:37 +1100

    using default netlify form instead

--- src/components/contact.js
+++ src/components/contact.js
@@ -19,24 +19,26 @@ export default class Contact extends React.Component {
 
   render() {
     return (
-        <form method="post" action="#">
-            <div className="field half first">
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-                <label htmlFor="email">Email</label>
-                <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-                <label htmlFor="message">Message</label>
-                <textarea name="message" id="message" rows="6"></textarea>
-            </div>
-            <ul className="actions">
-                <li><input type="submit" value="Send Message" className="special" /></li>
-                <li><input type="reset" value="Reset" /></li>
-            </ul>
-        </form>
+      <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>   
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Your Role: <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
     )
   }
 }
